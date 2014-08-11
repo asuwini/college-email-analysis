6daecd1e2a9489014ab1c0ef476442a92ee342f2
James <mish0116@umn.edu>
2014-08-10 23:58:42 -0400

    Now downloads all messages in label; writes to CSV

--- quickstart.py
+++ quickstart.py
@@ -5,13 +5,24 @@ by Google under the Apache 2.0 license. The docstrings
 for those functions cite the specific sources for
 those code samples.
 """
+from __future__ import print_function
 import httplib2
-
+import base64
+import email
+import unicodecsv
+import sys
+from BeautifulSoup import BeautifulSoup
 from apiclient.discovery import build
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import run
 from apiclient import errors
+
+def stderr(*args):
+    """
+    Convert arguments to string and print them to stderr
+    """
+    print(" ".join(str(a) for a in args), file=sys.stderr)
 
 
 def auth():
@@ -54,7 +65,7 @@ def get_threads(gmail_service):
     # Print ID for each thread
     if threads['threads']:
         for thread in threads['threads']:
-          print 'Thread ID: %s' % (thread['id'])
+          print('Thread ID: %s' % (thread['id']))
 
 
 def list_labels(service, user_id):
@@ -77,7 +88,7 @@ def list_labels(service, user_id):
         labels = response['labels']
         return labels
     except errors.HttpError, error:
-        print 'An error occurred: %s' % error
+        print('An error occurred: %s' % error)
 
 
 def list_messages_with_label(service, user_id, label_ids=[]):
@@ -113,7 +124,7 @@ def list_messages_with_label(service, user_id, label_ids=[]):
 
         return messages
     except errors.HttpError, error:
-        print 'An error occurred: %s' % error
+        print('An error occurred: %s' % error)
 
 
 def get_message_from_id(service, user_id, msg_id):
@@ -136,7 +147,34 @@ def get_message_from_id(service, user_id, msg_id):
             userId=user_id, id=msg_id).execute()
         return message
     except errors.HttpError, error:
-        print 'An error occurred: %s' % error
+        print('An error occurred: %s' % error)
+
+
+def get_mime_message_from_id(service, user_id, msg_id):
+    """Get a Message and use it to create a MIME Message.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        msg_id: The ID of the Message required.
+
+    Returns:
+        A MIME Message, consisting of data from Message.
+
+    Based on example code from
+    https://developers.google.com/gmail/api/v1/reference/users/messages/get
+    """
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id,
+                                                 format='raw').execute()
+        msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+
+        mime_msg = email.message_from_string(msg_str)
+
+        return mime_msg
+    except errors.HttpError, error:
+        print('An error occurred: %s' % error)
 
 
 def get_label_id_from_name(service, user, label_name):
@@ -154,13 +192,66 @@ def get_label_id_from_name(service, user, label_name):
     else:
         return False
 
-def main():
-    gmail_service = auth()
-    #print list_labels(gmail_service, "me")
-    #print list_messages_with_label(auth(), "me", "SENT")
-    label = get_label_id_from_name(gmail_service, "me", "College Apps")
-    messages = list_messages_with_label(gmail_service, "me", [label])
-    print get_message_from_id(gmail_service, "me", messages[0]['id'])
+def concat_email_text(mime_msg):
+    """
+    if mime_msg.is_multipart():
+        sub_messages = mime_msg.get_payload(decode=True)
+        if sub_messages is not None:
+            return "\n".join(concat_email_text(msg) for msg in sub_messages)
+        else:
+            return mime_
+    else:
+        return mime_msg.get_payload(decode=True)
+    """
+    text = ""
+    for part in mime_msg.walk():
+        payload = part.get_payload(decode=True)
+        if payload is not None:
+            text += "\n"
+            text += payload
+    return text
 
+
+def html_to_text(html_page_string):
+    """
+    Takes a full HTML document as a string and returns
+    the text within the <body>
+    """
+    soup = BeautifulSoup(html_page_string)
+    # `separator=" "` replaces `<br>` tags with " "
+    # preventing `James<br>Mishra` from being turned into
+    # `JamesMishra`
+    if soup.body is not None:
+        text = soup.body.getText(separator=" ")
+    else:
+        text = soup.getText(separator=" ")
+    # Remove all non-breaking spaces with regular ones
+    text = text.replace("&nbsp;", " ")
+    # Remove multiple spaces with single spaces
+    return " ".join(text.split())
+
+
+def main():
+    if len(sys.argv) > 1:
+        OUTPUT_CSV_FILE = sys.argv[1]
+    else:
+        OUTPUT_CSV_FILE = "output.csv"
+    gmail_service = auth()
+    label = get_label_id_from_name(gmail_service, "me", "College spam")
+    messages = list_messages_with_label(gmail_service, "me", [label])
+    for msg in messages:
+        mime_msg = get_mime_message_from_id(gmail_service, "me", msg['id'])
+        try:
+            text = html_to_text(concat_email_text(mime_msg))
+        except UnicodeEncodeError:
+            stderr("Skipping an email")
+            stderr("Subject line", mime_msg.get("Subject"))
+            continue
+        subject = mime_msg.get("Subject")
+        sender_domain = mime_msg.get("From").split("@")[1].split(">")[0]
+        csv_line = [sender_domain, subject, text]
+        with open(OUTPUT_CSV_FILE, 'a') as handle:
+            writer = unicodecsv.writer(handle)
+            writer.writerow(csv_line)
 if __name__ == "__main__":
     main()
